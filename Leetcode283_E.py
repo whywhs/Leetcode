31f2fd1fc8da9259d5d2ea1f796a29b8702a4101
Hongyu Wang <46738358+whywhs@users.noreply.github.com>
2020-02-05 23:08:40 +0800

    Update Leetcode283_E.py

--- Leetcode283_E.py
+++ Leetcode283_E.py
@@ -1,3 +1,4 @@
+#采用两个标记i,j来进行解题。i按照顺序从前往后找第一个0，j从i开始，从前往后找第一个非0，然后交换i和j的数值。
 class Solution(object):
     def moveZeroes(self, nums):
         """
@@ -20,3 +21,16 @@ class Solution(object):
                     i=i+1
                 else:
                     i=i+1
+#从前往后进行索引，找到非0的数即与j进行交换。如果均非0，那么i和j同步往前走，如果i为0，那么j停下，i继续往前走，知道找到非0的数进行交换。
+#该算法的时间复杂度最低。
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j=j+1
