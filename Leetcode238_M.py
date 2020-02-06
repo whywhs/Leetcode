620d4d5cad8d73f83d1c0b7a44c767362aa0fa7e
Hongyu Wang <46738358+whywhs@users.noreply.github.com>
2020-02-06 16:52:45 +0800

    Update Leetcode238_M.py

--- Leetcode238_M.py
+++ Leetcode238_M.py
@@ -34,3 +34,34 @@ class Solution(object):
             result_all.append(multi(a,list_b))
             
         return result_all[1:]+[result_all[0]]
+
+#这个做法是标准的做法。用两个list来分别存储nums中前向和后向的乘积。则结果即前向的[i-1]与后向的[i+1]的乘积。
+#边界条件需要进行考虑。
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        before = []
+        last = []
+        be=1
+        la=1
+        for i in nums:
+            be = be*i
+            before.append(be)
+        
+        for j in nums[::-1]:
+            la = la*j
+            last.append(la)
+        last = last[::-1]
+        
+        result=[]
+        for k in range(len(nums)):
+            if k-1<0:
+                result.append(last[k+1])
+            elif k+1==len(nums):
+                result.append(before[k-1])
+            else:
+                result.append(last[k+1]*before[k-1])
+        return result
