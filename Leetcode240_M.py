d2c5521fe62d2e0bb41b949fece3ee2a136dbd72
Hongyu Wang <46738358+whywhs@users.noreply.github.com>
2020-02-07 20:02:08 +0800

    Update Leetcode240_M.py

--- Leetcode240_M.py
+++ Leetcode240_M.py
@@ -1,3 +1,6 @@
+#对于这种数组本身已经排列好的，很方便的一个方法就是从右上角开始。如果target比start小，则向左移动，因为下方一列肯定是都大的。
+#如果target比start大，则向下移动，因为左边这一行肯定都是小的。
+#所以，当满足边界条件时就可以输出False，如果途中查到就输出True.
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
