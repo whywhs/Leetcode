cd6495a53e8fb89ba1dfe2d61984d721e5fcf138
Hongyu Wang <46738358+whywhs@users.noreply.github.com>
2020-02-27 23:33:03 +0800

    Create Leetcode297_H.py

---
+++ Leetcode297_H.py
@@ -0,0 +1,39 @@
+# 二叉树的序列化与反序列化。使用前序排列的方法。
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return '#'
+        return str(root.val)+','+self.serialize(root.left)+','+self.serialize(root.right)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        list_all = data.split(',')
+        root = self.func(list_all)
+        return root
+        
+        
+    def func(self,list_all):
+        val = list_all.pop(0)
+        if val=='#':
+            return None
+        val_node = TreeNode(val)
+        val_node.left = self.func(list_all)
+        val_node.right = self.func(list_all)
+        return val_node
