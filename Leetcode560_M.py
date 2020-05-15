eb5e19d90df2cb4798b14d872aafd2b2530a3305
Hongyu Wang <46738358+whywhs@users.noreply.github.com>
2020-05-15 11:44:35 +0800

    Update Leetcode560_M.py

--- Leetcode560_M.py
+++ Leetcode560_M.py
@@ -1,6 +1,10 @@
 # 和为k的子数组。这个题目使用了连续子数组和的做法。即对数组进行连续求和，每一个index之前的和均保存在dict中。
 # 当sum_now-k存在于dict中时，说明以sum_now-k的index为基准到当前index可以构成一个符合条件的数组，然后count累加在这之前的
 # 所有数组即可。初始的{0:1}代表的即当前值==k的情况。
+
+# 更正下，为什么可以采用连续数组和的形式进行count的计算，原因在于：1、对于当前下标i的数组和来说，sum_all代表了nums[:i]的和
+# 对于任意j，j<i的下标来说，num[:j]代表了到j的和，也即是在i之前所计算的连续和之一。那么nums[:i]-nums[:j]即代表了从j到i的子数组
+# 和。如果这个=k，说明count+1. 即当sum_all-nums[:j]==k，也即是sum_all-k==nums[:j]，而每个nums[:j]即存储在dict_r中。
 class Solution(object):
     def subarraySum(self, nums, k):
         """
